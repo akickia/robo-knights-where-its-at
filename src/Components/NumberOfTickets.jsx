8f1f67db987c57e801473a4330eaa069f919a193
jsundeq <116060345+jsundeq@users.noreply.github.com>
2023-04-14 14:31:46 +0200

    css added

--- src/Components/NumberOfTickets.jsx
+++ src/Components/NumberOfTickets.jsx
@@ -5,21 +5,16 @@ import PrimaryButton from './PrimaryButton'
 
 export default function NumberOfTickets({count, eventName, eventTime, totalPrice, decreaseCount, increaseCount}) {
 
-
-
-  
   return (
     <>
     <section className='event-price'>
-      {totalPrice && <h3>{totalPrice} sek</h3>}
+      {totalPrice && <h3 className='total-price'>{totalPrice} sek</h3>}
       {eventName && <h3>{eventName}</h3>}
       {eventTime && <h3>{eventTime}</h3>}
-      <p onClick={() => decreaseCount()}>-</p>
-      <p>{count}</p>
-      <p onClick={() => increaseCount()}>+</p>
+      <p className='decrease-ticket' onClick={() => decreaseCount()}>-</p>
+      <p className='ticket-count'>{count}</p>
+      <p className='increase-ticket' onClick={() => increaseCount()}>+</p>
     </section>      
-    
-
     </>
   )
 }
